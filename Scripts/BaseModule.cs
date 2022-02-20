1489eea1bf3fdd8082d0423ff003f540f57fe6cb
Dracon Interactive <rarceth@gmail.com>
2022-02-20 21:15:17 +0800

    full commit
    hopefully this works!

---
+++ Scripts/BaseModule.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dracon.Core
+{
+    public class BaseModule<T> : MonoBehaviour
+    {
+        public virtual void StartEx()
+        {
+
+        }
+
+        public virtual void UpdateEx()
+        {
+
+        }
+
+        public virtual void OnEnableEx()
+        {
+
+        }
+
+        public virtual void OnDisableEx()
+        {
+
+        }
+
+        public virtual void OnDestroyEx()
+        {
+
+        }
+    }
+}
+
